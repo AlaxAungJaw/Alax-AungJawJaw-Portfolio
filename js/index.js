0f67268588ba6a00ebc4551949ffa1ba5c305fc0
AlaxAungJaw <93766205+AlaxAungJaw@users.noreply.github.com>
2022-05-15 13:11:43 +0700

    Adding website files
    Adding site files to Github

---
+++ js/index.js
@@ -0,0 +1,30 @@
+// nav toggle 
+
+const navToggle = document.querySelector('.nav-toggle');
+const navLinks = document.querySelectorAll('.nav__link');
+
+navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+});
+
+navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
+}); 
+
+// scroll to top 
+
+var scrollToTopBtn = document.getElementById("scrollToTopBtn");
+var rootElement = document.documentElement;
+
+function scrollToTop() {
+  // Scroll to top logic
+  rootElement.scrollTo({
+    top: 0,
+    behavior: "smooth"
+  });
+}
+scrollToTopBtn.addEventListener("click", scrollToTop);
+
+
